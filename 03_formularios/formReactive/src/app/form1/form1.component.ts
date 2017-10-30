04d5a500211eb474d95c8cf16326a5410e4795ef
mc2000 <msaucedo2000@gmail.com>
2017-10-30 17:29:08 +0100

    formularios reactivos

---
+++ 03_formularios/formReactive/src/app/form1/form1.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-form1',
+  templateUrl: './form1.component.html',
+  styleUrls: ['./form1.component.css']
+})
+
+
+export class Form1Component implements OnInit {
+
+   public formGroup1 : FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    
+    this.formGroup1 = this.formBuilder.group({
+      titulo: ['',  Validators.required],
+      autor: ['', Validators.required],
+      editorial: [],
+      fecha: ['2017', [Validators.maxLength(4), Validators.required]] 
+    });
+    console.log (this.formGroup1);
+  }
+ 
+  borrar() 
+  {
+
+  }
+  enviar() 
+  {
+    
+    console.log (this.formGroup1);
+  }
+}
